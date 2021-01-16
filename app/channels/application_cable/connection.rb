a32d71be9f040da4277a4d66364796228d7b9e56
74905 <hurandall@yahoo.co.jp>
2021-01-16 10:59:02 +0000

    細かい変更

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,12 +9,13 @@ module ApplicationCable
 
     private
   def find_verified_user
-  (current_user = env['warden'].user) ? current_user : reject_unauthorized_connection
-      #   session_key = cookies.encrypted[Rails.application.config.session_options[:key]]
-      #   verified_id = session_key['warden.user.user.key'][0][0]
-      #   verified_user = User.find_by(id: verified_id)
-      #   return reject_unauthorized_connection unless verified_user
-      #   verified_user
+  #(current_user = env['warden'].user) ? current_user : reject_unauthorized_connection
+
+        session_key = cookies.encrypted[Rails.application.config.session_options[:key]]
+        verified_id = session_key['warden.user.user.key'][0][0]
+        verified_user = User.find_by(id: verified_id)
+        return reject_unauthorized_connection unless verified_user
+        verified_user
   end
   end
 end
