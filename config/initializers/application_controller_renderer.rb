bc9ad45412d82f63279ef1fdb6e7334ea21d7f6b
74905 <73700673+74905@users.noreply.github.com>
2021-01-17 14:36:23 +0900

    Revert "Layout"

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,4 +6,3 @@
 #     https: false
 #   )
 # end
-Refile.secret_key = 'c2725ec4401a367ac52fea47a84762a2ac664121de69e4f300c0dbc0f5d2513960b3a1a3b6ae2205144582d0b452a91a99785bff68704dacdb3a3379e98c7fb0'
--- test/jobs/chat_broadcast_job_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ChatBroadcastJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
