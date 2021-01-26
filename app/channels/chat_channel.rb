61cb70e36c6d7e118a2f84c6a3f57de5a556ca71
74905 <hurandall@yahoo.co.jp>
2021-01-26 17:34:05 +0000

    調整

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -10,14 +10,14 @@ class ChatChannel < ApplicationCable::Channel
   def speak(data)
       #ActionCable.server.broadcast 'chat_channel', chat: data['chat']
       chat = Chat.create! message: data['chat'], user_id: current_user.id, room_id: params['room_id']
-      ActionCable.server.broadcast "chat_channel_#{params['room_id']}", chat: render_chat(chat, current_user.id)
+      ActionCable.server.broadcast "chat_channel_#{params['room_id']}", chat: render_chat(chat)
    
   end
   
   private
   
-  def render_chat(chat, user_id)
-    ApplicationController.render_with_signed_in_user(User.find(user_id), partial: 'chats/chat', locals: { chat: chat})
+  def render_chat(chat)
+    ApplicationController.renderer.render( partial: 'chats/chat', locals: { chat: chat, current_user: current_user})
     #ここで追加したいHTMLのテンプレートをコントローラーからとってくる　レシーブの際にデータを取りに行かなくていい　追記　current_userが取れないので、コントローラーに新たに定義
   end
 end
