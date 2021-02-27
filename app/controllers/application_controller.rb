9aa232fa4f473296c47e0e98f8b2ab50c6a104d5
74905 <hurandall@yahoo.co.jp>
2021-02-27 13:40:36 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,6 @@ class ApplicationController < ActionController::Base
   
   
  protected
-  def self.render_with_signed_in_user(user, *args)
-  ActionController::Renderer::RACK_KEY_TRANSLATION['warden'] ||= 'warden'
-  proxy = Warden::Proxy.new({}, Warden::Manager.new({})).tap{|i| i.set_user(user, scope: :user) }
-  renderer = self.renderer.new('warden' => proxy)
-  renderer.render(*args)
-  end
  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #ここではとりあえず名前を足した、DBには他のカラムも入れてます
